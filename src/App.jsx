5d9442ec9ed699c27872045de9b5a8f971131aef
betzfer <miguelrodriguesdeoliveirasenai@gamil.com>
2023-11-16 16:52:54 -0300

    daqui pra frente so pra baixo

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import AppBar from "./assets/components/AppBar/AppBar";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>
+        <AppBar />
+      </div>
+    </>
+  );
+}
+
+export default App;
